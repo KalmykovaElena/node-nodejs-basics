a786000c0bddcfcef2f18b1e8f38ba27d5a210d9
Elena <kalmykhovae@gmail.com>
2024-01-29 19:33:42 +0300

    add solution Streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __filePath = "/files/fileToRead.txt";
+
 const read = async () => {
-    // Write your code here 
+  const fullPath = path.join(__dirname, __filePath);
+  const readStream = fs.createReadStream(fullPath);
+  readStream
+    .on("error", () => {
+      throw Error("FS operation failed");
+    })
+    .pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __filePath = "/files/fileToWrite.txt";
+
 const write = async () => {
-    // Write your code here 
+  const fullPath = path.join(__dirname, __filePath);
+  try {
+    const writeStream = fs.createWriteStream(fullPath);
+    process.stdin.pipe(writeStream);
+  } catch (err) {
+    throw Error("FS operation failed");
+  }
 };
 
 await write();
