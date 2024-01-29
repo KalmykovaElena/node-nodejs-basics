2ef9ecd619d40b579fb8574b78caab8a108b402d
Elena <kalmykhovae@gmail.com>
2024-01-29 20:15:42 +0300

    add solution Zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import fs from "fs";
+import path from "path";
+import zlib from "zlib";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __filePath = "/files/fileToCompress.txt";
+const __compressPath = "/files/archive.gz";
 const compress = async () => {
-    // Write your code here 
+  const fullPath = path.join(__dirname, __filePath);
+  const fullCompressPath = path.join(__dirname, __compressPath);
+  try {
+    const readStream = fs.createReadStream(fullPath, {
+      encoding: "utf8",
+    });
+    const writeStream = fs.createWriteStream(fullCompressPath);
+    const compressStream = zlib.createGzip();
+
+    readStream.pipe(compressStream).pipe(writeStream);
+  } catch (err) {
+    throw Error("FS operation failed");
+  }
 };
 
 await compress();
